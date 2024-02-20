bf525beda0abbfdec4b86ff70cb27770279c502a
KuroGuiri <iotglider@gmail.com>
2024-02-20 20:45:13 +0600

    Some code in class theme

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-#some code
-print("Some code")
+class Avengers:
+    Shield = []
+    name = "Nub"
+    power = 1
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+        print(self.name,"become Avenger! with a power equal:", self.power)
+        Avengers.Shield.append(self)
+
+    @staticmethod
+    def generalFee():
+        print()
+        for avenger in Avengers.Shield:
+            print("| The ",avenger.name)
+        print("\nAll Avengers become in general fee!")
+
+
+AV1 = Avengers("Klark", 99)
+AV2 = Avengers("Halk", 101)
+AV3 = Avengers("IronMan", 77)
+AV4 = Avengers("Arrow", 0)
+
+Avengers.generalFee()

0c3f637cedea4d81ccb336e0c70d3d9cf8a2be97
KuroGuiri <iotglider@gmail.com>
2024-03-18 17:15:35 +0600

    Инкапсуляция

--- main.py
+++ main.py
@@ -2,17 +2,28 @@ class Avengers:
     Shield = []
     name = "Nub"
     power = 1
+
     def __init__(self, name, power):
-        self.name = name
-        self.power = power
-        print(self.name,"become Avenger! with a power equal:", self.power)
+        self.__name = name
+        self.__power = power
+        print(self.__name, "become Avenger! with a power equal:", self.__power)
         Avengers.Shield.append(self)
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    def print_person(self):
+        print(f"Имя: {self.__name}\tСила: {self.__power}")
 
     @staticmethod
     def generalFee():
         print()
         for avenger in Avengers.Shield:
-            print("| The ",avenger.name)
+            print("| The ", avenger.__name)
         print("\nAll Avengers become in general fee!")
 
 
@@ -22,3 +33,7 @@ AV3 = Avengers("IronMan", 77)
 AV4 = Avengers("Arrow", 0)
 
 Avengers.generalFee()
+
+AV2.name = "Havchik"
+AV2.print_person()
+print(AV2.name)
